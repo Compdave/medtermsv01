452d9661143111b48389fd6d938d2b2853459b2a
Compdave <compdave@comcast.net>
2026-08-16 17:15:35 -0400

    Update home_screen.dart

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -372,7 +372,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                   ),
                   Switch(
                     value: _timerEnabled,
-                    activeThumbColor: AppColors.primary,
+                    activeColor: AppColors.primary,
                     onChanged: (val) async {
                       setDialogState(() {});
                       await _setTimerPref(val);
